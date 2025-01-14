ce70e0c41eeaf5339f7c93ddb2edef2e70da747c
David <hunter0@gmail.com>
2025-01-14 21:33:35 +1100

    Update 15-config-postfix.sh
    added POSTFIX_RELAY_ENCRYPTION env setting for SSL option for SMTP relay

--- rootfs/etc/cont-init.d/15-config-postfix.sh
+++ rootfs/etc/cont-init.d/15-config-postfix.sh
@@ -167,14 +167,14 @@ smtp_tls_ciphers = high
 smtp_tls_mandatory_exclude_ciphers = MD5, DES, ADH, RC4, PSD, SRP, 3DES, eNULL, aNULL
 smtp_tls_exclude_ciphers = MD5, DES, ADH, RC4, PSD, SRP, 3DES, eNULL, aNULL
 EOL
-  if [ "$POSTFIX_RELAY_ENCRYPTION" = "TLS"]; then
-    cat >>/etc/postfix/main.cf <<EOL
-smtp_tls_security_level = may
-EOL
-  elif [ "$POSTFIX_RELAY_ENCRYPTION" = "SSL"]; then
+  if [ "$POSTFIX_RELAY_ENCRYPTION" = "SSL"]; then
     cat >>/etc/postfix/main.cf <<EOL
 smtp_tls_wrappermode = yes
 smtp_tls_security_level = encrypt
+EOL
+  else
+    cat >>/etc/postfix/main.cf <<EOL
+smtp_tls_security_level = may
 EOL
   fi
 fi
